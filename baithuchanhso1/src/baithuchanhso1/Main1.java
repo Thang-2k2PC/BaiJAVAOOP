01b68c485751fe5710f9583398616638ec9afc5d
Thắng Pc <phamminhthang752002@gmail.com>
2022-01-17 18:34:24 +0700

    first commit

---
+++ baithuchanhso1/src/baithuchanhso1/Main1.java
@@ -0,0 +1,11 @@
+package baithuchanhso1;
+
+/**
+ *
+ * @author DELL
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ baithuchanhso2/src/baithuchanhso2/Main.java
@@ -0,0 +1,23 @@
+package baithuchanhso2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DELL
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String std ;
+        System.out.println("Nhap ten: ");
+        std = input.nextLine();
+        System.out.println("Xin chao " + std);
+        
+    }
+    
+}
